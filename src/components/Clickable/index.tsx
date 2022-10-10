24b0353be1ec0f11e55c51d0539115ccb765a8d1
Wan Wan <lf2com.bt@gmail.com>
2022-10-10 23:04:49 +0800

    feat: Support disabled for clickable component

--- src/components/Clickable/index.tsx
+++ src/components/Clickable/index.tsx
@@ -1,33 +1,45 @@
 import classnames from 'classnames';
 import React, {
-  FunctionComponent, MouseEventHandler, PropsWithChildren, useCallback,
+  FunctionComponent, MouseEventHandler, PropsWithChildren, useCallback, useMemo,
 } from 'react';
 
 import styles from './styles.module.scss';
 
 interface ClickableProps {
+  disabled?: boolean;
   className?: string;
   onClick?: MouseEventHandler;
   stopPropagation?: boolean;
 }
 
 const Clickable: FunctionComponent<PropsWithChildren<ClickableProps>> = ({
+  disabled = false,
   className,
   onClick,
   stopPropagation,
   ...restProps
 }) => {
+  const finalClassName = useMemo(() => classnames(
+    styles.clickable,
+    {
+      [styles.disabled]: disabled,
+    },
+    className,
+  ), [className, disabled]);
   const handleClick = useCallback<MouseEventHandler>((event) => {
     if (stopPropagation) {
       event.stopPropagation();
     }
+    if (disabled) {
+      return;
+    }
 
     onClick?.(event);
-  }, [onClick, stopPropagation]);
+  }, [disabled, onClick, stopPropagation]);
 
   return (
     <span
-      className={classnames(styles.clickable, className)}
+      className={finalClassName}
       onClick={handleClick}
       {...restProps}
     />
