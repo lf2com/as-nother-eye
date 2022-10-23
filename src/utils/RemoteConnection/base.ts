d9436c73de7aa5e7d7e7dc3e4f77c357bbdafa4f
Wan Wan <lf2com.bt@gmail.com>
2022-10-23 22:48:51 +0800

    refactor: Update remote connection

---
+++ src/utils/RemoteConnection/base.ts
@@ -0,0 +1,31 @@
+import Peer, { DataConnection } from 'peerjs';
+
+import Logger from '../logger';
+import randomStr from '../random';
+
+export class RemoteConnection {
+  protected selfId: string;
+
+  protected logger: Logger;
+
+  protected selfIsOnline = false;
+
+  protected selfPeer?: Peer;
+
+  protected connection?: DataConnection;
+
+  constructor(id: string = randomStr()) {
+    this.selfId = id;
+    this.logger = new Logger({
+      tag: `RC<${id}>`,
+    });
+  }
+
+  get peer() { return this.selfPeer; }
+
+  get id() { return this.selfId; }
+
+  get isOnline() { return this.selfIsOnline; }
+}
+
+export default RemoteConnection;
---
+++ src/utils/RemoteConnection/index.ts
@@ -0,0 +1,6 @@
+import './connect';
+import { RemoteConnection } from './base';
+
+import './event';
+
+export default RemoteConnection;
