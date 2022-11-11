6418940c6d6d08ed71c2efc96e073466b4ee20e4
Wan Wan <lf2com.bt@gmail.com>
2022-11-11 21:36:00 +0800

    feat: Add connector status and info

--- src/contexts/ConnectionContext/index.tsx
+++ src/contexts/ConnectionContext/index.tsx
@@ -1,17 +1,26 @@
+import { DataConnection, MediaConnection } from 'peerjs';
 import React, {
-  createContext, FunctionComponent, PropsWithChildren, useContext, useEffect, useMemo, useState,
+  createContext, FunctionComponent, PropsWithChildren, useCallback,
+  useContext, useEffect, useMemo, useState,
 } from 'react';
 
 import RemoteConnection from '../../utils/RemoteConnection';
+import EventHandler from '../../utils/RemoteConnection/event/handler';
 
 interface ConnectionContextProps {
   connector: RemoteConnection;
   isOnline: boolean;
+  isDataConnected: boolean;
+  isMediaConnected: boolean;
+  peerId: string | null;
 }
 
 const ConnectionContext = createContext<ConnectionContextProps>({
   connector: new RemoteConnection(),
   isOnline: false,
+  isDataConnected: false,
+  isMediaConnected: false,
+  peerId: null,
 });
 
 const ConnectionContextProvider: FunctionComponent<PropsWithChildren> = ({
@@ -21,24 +30,44 @@ const ConnectionContextProvider: FunctionComponent<PropsWithChildren> = ({
   const id = useMemo(() => searchParams.get('id') ?? undefined, [searchParams]);
   const connector = useMemo(() => new RemoteConnection(id), [id]);
   const [isOnline, setIsOnline] = useState(false);
+  const [peerId, setPeerId] = useState<string>();
+  const [dataConnection, setDataConnection] = useState<DataConnection>();
+  const [mediaConnection, setMediaConnection] = useState<MediaConnection>();
+
+  const onOnline = useCallback<EventHandler['online']>(() => setIsOnline(true), []);
+
+  const onOffline = useCallback<EventHandler['offline']>(() => setIsOnline(false), []);
+
+  const onConnectedData = useCallback<EventHandler['connecteddata']>((sourceId, connection) => {
+    setPeerId(sourceId);
+    setDataConnection(connection);
+  }, []);
+
+  const onConnectedMedia = useCallback<EventHandler['connectedmedia']>((_, connection) => {
+    setMediaConnection(connection);
+  }, []);
 
   const contextValue = useMemo<ConnectionContextProps>(() => ({
     connector,
     isOnline,
-  }), [connector, isOnline]);
+    isDataConnected: !!dataConnection,
+    isMediaConnected: !!mediaConnection,
+    peerId: peerId ?? null,
+  }), [connector, dataConnection, isOnline, mediaConnection, peerId]);
 
   useEffect(() => {
-    const onOnline = () => setIsOnline(true);
-    const onOffline = () => setIsOnline(false);
-
     connector.addEventListener('online', onOnline);
     connector.addEventListener('offline', onOffline);
+    connector.addEventListener('connecteddata', onConnectedData);
+    connector.addEventListener('connectedmedia', onConnectedMedia);
 
     return () => {
       connector.removeEventListener('online', onOnline);
       connector.removeEventListener('offline', onOffline);
+      connector.removeEventListener('connecteddata', onConnectedData);
+      connector.removeEventListener('connectedmedia', onConnectedMedia);
     };
-  }, [connector]);
+  }, [connector, onConnectedData, onConnectedMedia, onOffline, onOnline]);
 
   return (
     <ConnectionContext.Provider value={contextValue}>
