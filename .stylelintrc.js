27eb5790cebcefec4ee631082733f5e6458c210e
Wan Wan <lf2com.bt@gmail.com>
2022-11-13 00:06:28 +0800

    chore: Add eslint and stylelint rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,15 +6,22 @@ module.exports = {
   rules: {
     'color-function-notation': null,
     'alpha-value-notation': null,
+    'no-empty-first-line': true,
+    'block-no-empty': true,
     'block-opening-brace-space-before': 'always',
     'block-opening-brace-newline-after': 'always',
+    'block-closing-brace-newline-before': 'always',
     'block-closing-brace-newline-after': 'always',
+    'block-closing-brace-empty-line-before': 'never',
     'declaration-block-semicolon-newline-after': 'always',
     'no-missing-end-of-source-newline': true,
-    'rule-empty-line-before': 'always-multi-line',
+    // 'rule-empty-line-before': ['always-multi-line', {
+      // except: {
+      //   'first-nested': 'always',
+      // },
+    // }],
     'max-empty-lines': 1,
-    'block-no-empty': true,
-    'block-closing-brace-empty-line-before': 'never',
+    'indentation': 2,
     'selector-pseudo-class-no-unknown': [
       true,
       {
