7909d9a72b1b121b6ffe28081ea0881dc1ab1417
Wan Wan <lf2com.bt@gmail.com>
2022-12-09 22:51:13 +0800

    feat: Remove flow of connecting on welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,39 +1,13 @@
-import React, {
-  ReactNode, useCallback, useEffect, useState,
-} from 'react';
+import React, { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import { useConnectionContext } from '../../contexts/ConnectionContext';
-import { useModalContext } from '../../contexts/ModalContext';
 
 import Button from '../../components/Button';
 import Frame from '../../components/Frame';
-import Loading from '../../components/Loading';
-
-import delayAwaitResult from '../../utils/delayAwaitResult';
-import Logger from '../../utils/logger';
 
 import styles from './styles.module.scss';
 
-const logger = new Logger({ tag: '[Welcome]' });
-
 const Welcome = () => {
   const navigate = useNavigate();
-  const { connector, isOnline } = useConnectionContext();
-  const { notice } = useModalContext();
-  const [showLoading, setShowLoading] = useState(true);
-  const [LoadingMessage, setLoadingMessage] = useState<ReactNode>('Connecting to server');
-
-  const initConnector = useCallback(async () => {
-    try {
-      await delayAwaitResult(connector.connect(), 1000);
-      logger.log(`Connected to server: ${connector.id}`);
-      setLoadingMessage(`Connected to server: ${connector.id}`);
-      setShowLoading(false);
-    } catch (error) {
-      notice(`${error}`);
-    }
-  }, [connector, notice]);
 
   const handleAsPhotoer = useCallback(() => {
     navigate('/photoer');
@@ -43,22 +17,14 @@ const Welcome = () => {
     navigate('/camera');
   }, [navigate]);
 
-  useEffect(() => {
-    initConnector();
-  }, [initConnector]);
-
   return (
     <Frame className={styles.welcome}>
-      <Button disabled={!isOnline} onClick={handleAsPhotoer}>
+      <Button onClick={handleAsPhotoer}>
         As Photoer
       </Button>
-      <Button disabled={!isOnline} onClick={handleAsCamera}>
+      <Button onClick={handleAsCamera}>
         As Camera
       </Button>
-
-      <Loading show={showLoading}>
-        {LoadingMessage}
-      </Loading>
     </Frame>
   );
 };
