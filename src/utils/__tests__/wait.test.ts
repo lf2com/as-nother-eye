05974b64e06025296c5a9d279c2ec0235d944311
Wan Wan <lf2com.bt@gmail.com>
2023-01-07 16:55:00 +0800

    test: Check wait return value

--- src/utils/__tests__/wait.test.ts
+++ src/utils/__tests__/wait.test.ts
@@ -5,6 +5,10 @@ const flushPromise = () => new Promise((resolve) => {
 });
 
 describe('wait', () => {
+  it('should return a promise', () => {
+    expect(wait(0)).toBeInstanceOf(Promise);
+  });
+
   it('should wait for 1000 ms', async () => {
     const after = jest.fn();
 
