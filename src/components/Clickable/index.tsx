7cb19fd78ec65acaa82bd38fec1a6fd6fdc56851
Wan Wan <lf2com.bt@gmail.com>
2024-09-02 11:15:27 +0800

    refactor: remove Frame and use button as Clickable base (#40)

--- src/components/Clickable/index.tsx
+++ src/components/Clickable/index.tsx
@@ -1,50 +1,24 @@
-import classNames from 'classnames';
-import type {
-  ComponentProps,
-  FC,
-  MouseEventHandler,
-  PropsWithChildren,
-} from 'react';
-import React, { useCallback } from 'react';
+import type { ComponentProps, FC, PropsWithChildren } from 'react';
+import React from 'react';
 
-interface ClickableProps extends ComponentProps<'span'> {
-  disabled?: boolean;
+import twClassNames from '@/utils/twClassNames';
+
+interface ClickableProps extends ComponentProps<'button'> {
   stopPropagation?: boolean;
 }
 
 const Clickable: FC<PropsWithChildren<ClickableProps>> = ({
-  disabled = false,
   className,
-  onClick,
-  stopPropagation,
   ...restProps
-}) => {
-  const handleClick = useCallback<MouseEventHandler<HTMLSpanElement>>(
-    event => {
-      if (stopPropagation) {
-        event.stopPropagation();
-      }
-      if (!disabled) {
-        onClick?.(event);
-      }
-    },
-    [disabled, onClick, stopPropagation]
-  );
-
-  return (
-    <span
-      className={classNames(
-        '[-webkit-tap-highlight-color:transparent] select-none bg-black/0 cursor-pointer z-[1]',
-        {
-          'cursor-not-allowed': disabled,
-          'active:brightness-90': !disabled,
-        },
-        className
-      )}
-      onClick={handleClick}
-      {...restProps}
-    />
-  );
-};
+}) => (
+  <button
+    className={twClassNames(
+      '[-webkit-tap-highlight-color:transparent] select-none bg-black/0 cursor-pointer z-[1]',
+      'disabled:cursor-not-allowed [&:not(:disabled)]:active:brightness-90',
+      className
+    )}
+    {...restProps}
+  />
+);
 
 export default Clickable;
--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,11 +1,9 @@
 import type { FC, PropsWithChildren } from 'react';
 import React from 'react';
 
-import WithTextShadow from '@/components/WithTextShadow';
-
 const Tag: FC<PropsWithChildren> = ({ children }) => (
-  <div className="p-[0.5em] text-[1rem] text-black">
-    <WithTextShadow>{children}</WithTextShadow>
+  <div className="p-[0.5em] text-[1rem] text-black text-shadow-white">
+    {children}
   </div>
 );
 
