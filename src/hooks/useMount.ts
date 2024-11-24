863d0bf3b4e4fcd4a571f122c9bdea8e078f5be6
Wan Wan <lf2com.bt@gmail.com>
2024-11-25 00:00:48 +0800

    feat: add useMount/useUnmount hook and test

--- src/hooks/useMount.ts
+++ src/hooks/useMount.ts
@@ -2,7 +2,11 @@ import { useEffect } from 'react';
 
 const useMount = (fn: Parameters<typeof useEffect>[0]) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  return useEffect(fn, []);
+  useEffect(fn, []);
+};
+
+export const useUnmount = (fn: ReturnType<Parameters<typeof useMount>[0]>) => {
+  useMount(() => fn);
 };
 
 export default useMount;
