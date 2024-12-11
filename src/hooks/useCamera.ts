15841bb30f7c5afcaa6989949059b5c97a61fab0
Wan Wan <lf2com.bt@gmail.com>
2024-12-11 23:45:59 +0800

    feat: add hook for handling cameras

---
+++ src/hooks/useCamera.ts
@@ -0,0 +1,66 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+import useMount from './useMount';
+
+const useCamera = () => {
+  const [cameras, setCameras] = useState<MediaDeviceInfo[]>([]);
+  const [cameraIndex, setCameraIndex] = useState(0);
+  const [cameraStream, setCameraStream] = useState<MediaStream | null>(null);
+
+  const camera = useMemo<MediaDeviceInfo | null>(
+    () => cameras[cameraIndex] ?? null,
+    [cameraIndex, cameras]
+  );
+
+  const switchCamera = useCallback(() => {
+    setCameraIndex(prevIndex => (prevIndex + 1) % cameras.length);
+  }, [cameras.length]);
+
+  useMount(() => {
+    const init = async () => {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const realCameras = devices.filter(
+        ({ kind, label }) =>
+          kind === 'videoinput' && !/\bvirtual\b/i.test(label)
+      );
+
+      setCameras(realCameras);
+    };
+
+    init();
+  });
+
+  useEffect(() => {
+    if (!camera) {
+      return;
+    }
+
+    let aborted = false;
+
+    const init = async () => {
+      const { deviceId } = camera;
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { deviceId },
+      });
+
+      if (!aborted) {
+        setCameraStream(stream);
+      }
+    };
+
+    init();
+
+    return () => {
+      aborted = true;
+      setCameraStream(null);
+    };
+  }, [camera]);
+
+  return {
+    switchCamera,
+    cameraStream,
+    camera,
+  };
+};
+
+export default useCamera;
