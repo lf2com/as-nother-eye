5fe450e005f394c8c205b47c12ff68fe1de11242
Wan Wan <lf2com.bt@gmail.com>
2024-12-22 23:36:18 +0800

    refactor: refine share flow

--- src/utils/shareData.ts
+++ src/utils/shareData.ts
@@ -1,13 +1,11 @@
 type ShareData = Required<Parameters<Navigator['canShare']>>[0];
 
-export const canShare = (data: ShareData) => navigator.canShare(data);
-
 const shareData = async (data: ShareData) => {
-  if (!canShare(data)) {
-    throw Error('Cannot share data');
+  if (!navigator.canShare(data)) {
+    throw Error('Unable to share');
   }
 
-  await navigator.share(data);
+  return navigator.share(data);
 };
 
 export default shareData;
