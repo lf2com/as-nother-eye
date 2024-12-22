b8229fa6fa27466748408d0eb0d1291fa135dd90
Wan Wan <lf2com.bt@gmail.com>
2024-12-22 23:15:33 +0800

    refactor: refine common components of button and clickable

--- src/components/Clickable/index.tsx
+++ src/components/Clickable/index.tsx
@@ -3,18 +3,29 @@ import React from 'react';
 
 import twClassNames from '@/utils/twClassNames';
 
-interface ClickableProps extends ComponentProps<'button'> {
+type ButtonProps = ComponentProps<'button'>;
+
+interface ClickableProps extends ButtonProps {
+  onClick: ButtonProps['onClick'];
   stopPropagation?: boolean;
+  isActive?: boolean;
 }
 
 const Clickable: FC<PropsWithChildren<ClickableProps>> = ({
   className,
+  disabled,
+  isActive,
   ...restProps
 }) => (
   <button
+    disabled={disabled}
     className={twClassNames(
       '[-webkit-tap-highlight-color:transparent] select-none bg-black/0 cursor-pointer',
-      'disabled:cursor-not-allowed [&:not(:disabled)]:active:brightness-95',
+      {
+        'cursor-not-allowed': disabled,
+        'active:brightness-90 pointer-events-auto': !disabled,
+        'brightness-90': isActive,
+      },
       className
     )}
     {...restProps}
